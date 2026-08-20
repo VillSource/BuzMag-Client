630ea8111c19801a7d0fcea38b51fe39046d91b3
VillSource <30929839+VillSource@users.noreply.github.com>
2026-08-20 09:47:44 +0700

    init shardcn

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,7 +18,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': '/src',
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
