b1359c1b023858ed045a32e4176dd1206adb54bc
VillSource <30929839+VillSource@users.noreply.github.com>
2026-08-24 00:46:33 +0700

    move to pages

--- src/routes/ping.$name.tsx
+++ src/routes/ping.$name.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import PingPage from '../features/ping'
+import PingPage from '../pages/ping'
 
 export const Route = createFileRoute('/ping/$name')({
   component: PingPage,
