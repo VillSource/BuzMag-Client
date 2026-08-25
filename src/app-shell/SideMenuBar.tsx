00af5657df3b6bbeddfd062860bde9a84844023f
VillSource <30929839+VillSource@users.noreply.github.com>
2026-08-26 02:28:59 +0700

    collap menu

--- src/app-shell/SideMenuBar.tsx
+++ src/app-shell/SideMenuBar.tsx
@@ -14,19 +14,22 @@ import {
 import { Skeleton } from "@/components/ui/skeleton";
 import { Link } from "@tanstack/react-router";
 import { useAppShell } from "./AppShell";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export const SideMenuBar = () => {
-  const { selectedPrime, selectedSec } = useAppShell();
+  const { selectedPrime, selectedSec, setMenuOpen, menuOpen } = useAppShell();
   return (
     <>
       <ShadcnSidebar
         variant="sidebar"
-        className="absolute inset-y-0 left-0 z-10 h-full min-h-0 overflow-hidden border-r"
+        className="absolute inset-y-0 left-0 z-10 h-full min-h-0  border-r group/collmenu min-w-fit"
         side="left"
       >
-        {/* <SidebarHeader className="h12 ps-4 border-b">
-          {selectedPrime?.lable}
-        </SidebarHeader> */}
         <SidebarContent>
           {!!selectedPrime?.menu &&
             selectedPrime.menu.map((group) => (
@@ -47,12 +50,43 @@ export const SideMenuBar = () => {
                 </SidebarGroupContent>
               </SidebarGroup>
             ))}
-
         </SidebarContent>
 
         <SidebarFooter>
           <MenuWidget />
         </SidebarFooter>
+        {menuOpen ? (
+          <div className="absolute flex items-center justify-center -right-3 w-6 h-full">
+            <Tooltip>
+              <TooltipTrigger
+                render={
+                  <ChevronLeft
+                    onClick={() => setMenuOpen(false)}
+                    className="cursor-pointer group-hover/collmenu:border group-hover/collmenu:w-3 m-auto bg-background border-0 rounded h-10 w-0 transition-all"
+                  />
+                }
+              ></TooltipTrigger>
+              <TooltipContent side="right">collapse</TooltipContent>
+            </Tooltip>
+          </div>
+        ) : (
+          !!selectedPrime?.menu &&
+          selectedPrime.menu.length > 0 && (
+            <div className="absolute flex items-center justify-center -right-6 w-6 h-full">
+              <Tooltip>
+                <TooltipTrigger
+                  render={
+                    <ChevronRight
+                      onClick={() => setMenuOpen(true)}
+                      className="cursor-pointer group-hover/collmenu:border-0 group-hover/collmenu:w-3 me-auto bg-background border-0 rounded h-10 w-0 transition-all"
+                    />
+                  }
+                ></TooltipTrigger>
+                <TooltipContent side="right">explan</TooltipContent>
+              </Tooltip>
+            </div>
+          )
+        )}
       </ShadcnSidebar>
     </>
   );
